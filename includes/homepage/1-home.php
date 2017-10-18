7c98153b0115d433571181eda3a24db49729f578
Andy <afairlie@gmail.com>
2017-10-18 20:31:21 +0200

    Mobile menu colours Specialisations animation

--- includes/homepage/1-home.php
+++ includes/homepage/1-home.php
@@ -7,62 +7,62 @@
 		<ol class="specialisations__menu menu-horz-flex">
 			<li class="specialisations__item">
 				<a href="#" class="js-specialisation-hover / specialisations__link" data-color="5e8c62">
-					<i class="sprite_graphic_design / specialisations__icon"></i>
-					<span class="h5 specialisations__title"> Graphic Design </span>
+					<i data-animation="specialisation-icon-1" class="sprite_graphic_design / specialisations__icon"></i>
+					<span data-animation="specialisation-title-1" class="h5 specialisations__title"> Graphic Design </span>
 				</a>
 			</li>
 			<li class="specialisations__item">
 				<a href="#" class="js-specialisation-hover / specialisations__link" data-color="8b1635">
-					<i class="sprite_multimedia / specialisations__icon"></i>
-					<span class="h5 specialisations__title"> Multimedia Design </span>
+					<i data-animation="specialisation-icon-2" class="sprite_multimedia / specialisations__icon"></i>
+					<span data-animation="specialisation-title-2" class="h5 specialisations__title"> Multimedia Design </span>
 				</a>
 			</li>
 			<li class="specialisations__item">
 				<a href="#" class="js-specialisation-hover / specialisations__link" data-color="bf5327">
-					<i class="sprite_copywriting / specialisations__icon"></i>
-					<span class="h5 specialisations__title"> Copywriting </span>
+					<i data-animation="specialisation-icon-3" class="sprite_copywriting / specialisations__icon"></i>
+					<span data-animation="specialisation-title-3" class="h5 specialisations__title"> Copywriting </span>
 				</a>
 			</li>
 			<li class="specialisations__item">
 				<a href="#" class="js-specialisation-hover / specialisations__link" data-color="5e8c62">
-					<i class="sprite_game_design / specialisations__icon"></i>
-					<span class="h5 specialisations__title"> Game Design </span>
+					<i data-animation="specialisation-icon-4" class="sprite_game_design / specialisations__icon"></i>
+					<span data-animation="specialisation-title-4" class="h5 specialisations__title"> Game Design </span>
 				</a>
 			</li>
 			<li class="specialisations__item">
 				<a href="#" class="js-specialisation-hover / specialisations__link" data-color="e8ae30">
-					<i class="sprite_fashion_design / specialisations__icon"></i>
-					<span class="h5 specialisations__title"> Fashion Design </span>
+					<i data-animation="specialisation-icon-5" class="sprite_fashion_design / specialisations__icon"></i>
+					<span data-animation="specialisation-title-5" class="h5 specialisations__title"> Fashion Design </span>
 				</a>
 			</li>
 			<li class="specialisations__item">
 				<a href="#" class="js-specialisation-hover / specialisations__link" data-color="578ea3">
-					<i class="sprite_interior_design / specialisations__icon"></i>
-					<span class="h5 specialisations__title"> Interior Design </span>
+					<i data-animation="specialisation-icon-6" class="sprite_interior_design / specialisations__icon"></i>
+					<span data-animation="specialisation-title-6" class="h5 specialisations__title"> Interior Design </span>
 				</a>
 			</li>
 			<li class="specialisations__item">
 				<a href="#" class="js-specialisation-hover / specialisations__link" data-color="8b1635">
-					<i class="sprite_digital_marketing / specialisations__icon"></i>
-					<span class="h5 specialisations__title"> Digital marketing </span>
+					<i data-animation="specialisation-icon-7" class="sprite_digital_marketing / specialisations__icon"></i>
+					<span data-animation="specialisation-title-7" class="h5 specialisations__title"> Digital marketing </span>
 				</a>
 			</li>
 			<li class="specialisations__item">
 				<a href="#" class="js-specialisation-hover / specialisations__link" data-color="e8ae30">
-					<i class="sprite_brand_communication / specialisations__icon"></i>
-					<span class="h5 specialisations__title"> Brand Communication </span>
+					<i data-animation="specialisation-icon-8" class="sprite_brand_communication / specialisations__icon"></i>
+					<span data-animation="specialisation-title-8" class="h5 specialisations__title"> Brand Communication </span>
 				</a>
 			</li>
 			<li class="specialisations__item">
 				<a href="#" class="js-specialisation-hover / specialisations__link" data-color="bf5327">
-					<i class="sprite_photography / specialisations__icon"></i>
-					<span class="h5 specialisations__title"> Photography </span>
+					<i data-animation="specialisation-icon-9" class="sprite_photography / specialisations__icon"></i>
+					<span data-animation="specialisation-title-9" class="h5 specialisations__title"> Photography </span>
 				</a>
 			</li>
 			<li class="specialisations__item">
 				<a href="#" class="js-specialisation-hover / specialisations__link" data-color="e8ae30">
-					<i class="sprite_brand_management / specialisations__icon"></i>
-					<span class="h5 specialisations__title"> Brand management </span>
+					<i data-animation="specialisation-icon-10" class="sprite_brand_management / specialisations__icon"></i>
+					<span i data-animation="specialisation-title-10" class="h5 specialisations__title"> Brand management </span>
 				</a>
 			</li>
 
