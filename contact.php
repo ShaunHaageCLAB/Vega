8eeba90ff5e3d2e248c74734e3a8e898b7d385f1
Andy <afairlie@gmail.com>
2017-10-25 19:10:32 +0200

    Contact form themeing and layout

--- contact.php
+++ contact.php
@@ -4,12 +4,24 @@
 <title>Contact Us</title>
 <?php include("includes/sharepoint/header.php"); ?>
 
-
 <?php include("includes/contact/banner.php"); ?>
-<div class="container">
-<?php include("includes/contact/details.php"); ?>
-<?php include("includes/contact/form.php"); ?>
+
+<div class="container theme-green">
+	<p class="text-center">
+		Please use the form below to contact us. We look forward to working with you and will get back to you as soon as possible. All fields are required.
+	</p>
+
+	<div class="row baseline-lg">
+		<div class="contactform__wrapper / col-lg-6 col-md-6">
+			<?php include("includes/contact/form.php"); ?>
+		</div>
+
+		<div class="contactdetails__wrapper / col-lg-6 col-md-6">
+			<?php include("includes/contact/details.php"); ?>
+		</div>
+	</div>
 </div>
+
 <?php include("includes/contact/map.php"); ?>
 
 
